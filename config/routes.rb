fb4949da2a3c2a704375ecdf56262d6c8aff59f6
山本　憲 <em072029@gmail.com>
2019-01-27 11:41:48 +0900

    投稿画面のcontroller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/create'
+  #
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
