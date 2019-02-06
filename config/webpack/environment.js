7ea538a0519ec2724ad9f08a100a1f5473081bcc
山本　憲 <em072029@gmail.com>
2019-02-06 17:23:26 +0900

    vue fileのCSSを反映できるようにした。

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,3 +3,5 @@ const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
 module.exports = environment
+// https://qiita.com/tocomi/items/0e1522418d0a21a6b291
+environment.loaders.get('vue').use[0].options.extractCSS = false
