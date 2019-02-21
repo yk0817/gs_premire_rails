0afac3d5b0c3179bd57fb4d8edca56fb7ea5bdf8
山本　憲 <em072029@gmail.com>
2019-02-22 07:37:24 +0900

    user作成テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "test" }
+    password { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  context "password create" do
-    it "" do
-      binding.pry
+  describe ".create" do
+    context "does not use Factory bot" do
+      it "create User" do
+        User.create(name: "hoge", password: "hoge")
+        expect(User.count).to eq 1
+      end
+    end
+
+    context "use Factory bot" do
+      before{ create(:user) }
+      it "User create" do
+        expect(User.count).to eq 1
+      end
+      # before節のスコープに注意
+      it "not eq count 0" do
+        expect(User.count).not_to eq 0
+      end
     end
   end
 end
